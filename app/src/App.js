b9c4a14a959d3f70d8d62aff47ad0d083309c514
anael <anael.mashinsky@gmail.com>
2020-09-22 15:06:44 +0300

    rest api asset established

--- app/src/App.js
+++ app/src/App.js
@@ -1,8 +1,76 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
+import flightsAPI from "./api/flightsAPI";
 
 function App() {
-  return <div className='App'>Flights Data:</div>;
+  const [localDate, setLocalDate] = useState("");
+  const [airportData, setAirportData] = useState("");
+
+  useEffect(() => {
+    flightsAPI.getAirport().then((response) => {
+      console.log(response);
+      setAirportData(response.data[0]);
+    });
+
+    const usaTime = new Date().toLocaleString("en-US", {
+      timeZone: "America/New_York",
+    });
+    console.log("USA time: " + usaTime);
+    setLocalDate({
+      date: new Date(usaTime).toISOString().slice(0, 10),
+      time: new Date().toLocaleTimeString([], {
+        hour: "2-digit",
+        minute: "2-digit",
+        timeZone: "America/New_York",
+      }),
+    });
+    // console.log("USA time: " + new Date(usaTime).toISOString().slice(0, 10));
+    // var string = new Date().toLocaleTimeString([], {
+    //   hour: "2-digit",
+    //   minute: "2-digit",
+    //   timeZone: "America/New_York",
+    // });
+    // console.log(string);
+  }, []);
+
+  useEffect(() => {
+    if (localDate !== "") {
+      flightsAPI
+        .getAllFlights(localDate.date, localDate.time)
+        .then((response) => {
+          console.log(response);
+        });
+    }
+  }, [localDate]);
+  // useEffect(() => {
+  //   const formattedDate = new Date(localDate.usaTime.toISOString());
+  //   let year = formattedDate.getFullYear();
+  //   let month = formattedDate.getMonth() + 1;
+  //   let dt = formattedDate.getDate();
+  //   if (dt < 10) {
+  //     dt = "0" + dt;
+  //   }
+  //   if (month < 10) {
+  //     month = "0" + month;
+  //   }
+  //   console.log(year + "-" + month + "-" + dt);
+  // }, [localDate]);
+
+  return (
+    <div className='App'>
+      {airportData && (
+        <div className='airport-info'>
+          <h2>
+            {airportData.airport_name} airport,
+            {airportData.city_iata_code} {airportData.country_name}
+          </h2>
+          <h3>Local time: {localDate.time}</h3>
+          <h3>Local date: {localDate.date}</h3>
+        </div>
+      )}
+      <h3>Flights Data:</h3>
+    </div>
+  );
 }
 
 export default App;
--- app/src/api/flightsAPI.js
+++ app/src/api/flightsAPI.js
@@ -1,1 +1,58 @@
-const API_KEY = process.env.REACT_APP_FLIGHTS_API_KEY;
+import axios from "axios";
+const BASE_URL = "http://api.aviationstack.com/v1";
+
+const getAllArrivingFlights = async () => {
+  const params = {
+    access_key: process.env.REACT_APP_API_KEY,
+    arr_iata: "JFK",
+  };
+  try {
+    const response = await axios.get(`${BASE_URL}/flights`, {
+      params,
+    });
+    return response.data;
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const getAllDepartingFlights = async () => {
+  const params = {
+    access_key: process.env.REACT_APP_API_KEY,
+    dep_iata: "JFK",
+  };
+  try {
+    const response = await axios.get(`${BASE_URL}/flights`, {
+      params,
+    });
+    return response.data;
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const getAllFlights = async (date, time) => {
+  console.log(date);
+  console.log(time);
+  const arrFlights = await getAllArrivingFlights();
+  const filteredFlights = arrFlights.data.filter((x) => x.flight_date === date);
+  console.log(filteredFlights.length);
+  const depFlights = await getAllDepartingFlights();
+  return { arrFlights, depFlights };
+};
+const getAirport = async () => {
+  const params = {
+    access_key: process.env.REACT_APP_API_KEY,
+    iata_code: "JFK",
+  };
+  try {
+    const response = await axios.get(`${BASE_URL}/airports`, {
+      params,
+    });
+    return response.data;
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export default { getAllFlights, getAirport };
